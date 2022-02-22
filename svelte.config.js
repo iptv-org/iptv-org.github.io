1f96bd03b7c8856b9e8e21b6226a018bc3895901
Arhey <github@oiya.xyz>
2022-02-22 11:55:28 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,10 @@ import adapter from '@sveltejs/adapter-static'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		})
 	}
 }
 
