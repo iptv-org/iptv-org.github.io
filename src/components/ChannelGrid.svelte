4f2faf3e8df6089c73dca9c4f0410b465dba842c
Arhey <github@oiya.xyz>
2022-09-13 22:05:32 +0300

    Update ChannelGrid.svelte

--- src/components/ChannelGrid.svelte
+++ src/components/ChannelGrid.svelte
@@ -16,23 +16,23 @@
 							<th scope="col" class="min-w-[10rem] md:w-[13rem] relative"></th>
 							<th
 								scope="col"
-								class="py-3 px-2 text-xs font-semibold tracking-wider text-left text-gray-400 uppercase dark:text-gray-400 min-w-[14rem] md:w-[18rem]"
+								class="py-3 px-3 text-xs font-semibold tracking-wider text-left text-gray-400 uppercase dark:text-gray-400 min-w-[14rem] md:w-[19rem]"
 							>
 								Name
 							</th>
 							<th
 								scope="col"
-								class="py-3 px-2 text-xs font-semibold tracking-wider text-left text-gray-400 uppercase dark:text-gray-400 min-w-[12rem] md:w-[18rem]"
+								class="py-3 px-2 text-xs font-semibold tracking-wider text-left text-gray-400 uppercase dark:text-gray-400 min-w-[12rem] md:w-[16rem]"
 							>
 								TVG-ID
 							</th>
-							<th scope="col" class="relative min-w-[13rem]">
+							<th scope="col" class="relative min-w-[11rem]">
 								<span class="sr-only">Actions</span>
 							</th>
 						</tr>
 					</thead>
 					<tbody class="bg-white dark:bg-gray-800">
-						{#each channels as channel}
+						{#each channels as channel (channel.id)}
 						<ChannelItem bind:channel="{channel}" />
 						{/each}
 					</tbody>
