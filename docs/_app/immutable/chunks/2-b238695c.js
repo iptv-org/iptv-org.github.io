dca1af7bbd3297e42abe20d7a9468bf15e6b30cd
Arhey <github@oiya.xyz>
2023-02-17 15:32:29 +0300

    Rebuild /docs

--- docs/_app/immutable/chunks/2-b238695c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b502142d.js";export{t as component};
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,7 @@
 const m = [
   "/_app/immutable/chunks/0-899ce9a5.js",
   "/_app/immutable/chunks/1-a05c0897.js",
-  "/_app/immutable/chunks/2-b238695c.js",
+  "/_app/immutable/chunks/2-8b90ddb2.js",
   "/_app/immutable/chunks/3-e5308f68.js",
   "/_app/immutable/assets/HTMLPreview-0d24e5da.css",
   "/_app/immutable/chunks/HTMLPreview-d8b21788.js",
@@ -9,20 +9,20 @@ const m = [
   "/_app/immutable/chunks/index-71936449.js",
   "/_app/immutable/chunks/singletons-228a5c53.js",
   "/_app/immutable/chunks/stores-eb5cf413.js",
-  "/_app/immutable/start-f15ffc58.js",
+  "/_app/immutable/start-061e7571.js",
   "/_app/immutable/components/error.svelte-d45d2747.js",
   "/_app/immutable/assets/_layout-32e929c2.css",
   "/_app/immutable/modules/pages/_layout.js-9cbb603b.js",
   "/_app/immutable/components/pages/_layout.svelte-a9bd6fbc.js",
   "/_app/immutable/assets/_page-dfa854c9.css",
-  "/_app/immutable/components/pages/_page.svelte-b502142d.js",
+  "/_app/immutable/components/pages/_page.svelte-90c41a04.js",
   "/_app/immutable/components/pages/channel/_page.svelte-d194e74b.js"
 ], h = [
   "/.nojekyll",
   "/favicon.png",
   "/logo_512.png",
   "/manifest.json"
-], o = "1676636073406", i = `cache_${o}`, p = m.concat(h), r = new Set(p);
+], o = "1676637121230", i = `cache_${o}`, p = m.concat(h), r = new Set(p);
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(i).then((s) => s.addAll(p)).then(() => {
