f3d4efea6bfc287713238e34f04a13a152b84b86
Arhey <github@oiya.xyz>
2023-05-04 02:32:11 +0300

    Rebuild /docs

--- docs/_app/immutable/chunks/2-77cd0b25.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-b1784de1.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-94a1d0f6.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-fc44449c.js";export{t as component};
