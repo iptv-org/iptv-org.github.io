156dd81f26b41d8ec90d2f96e8d92dcf720dc1f4
freearhey <7253922+freearhey@users.noreply.github.com>
2023-09-18 19:41:00 +0300

    Update +page.svelte

--- src/pages/channel/+page.svelte
+++ src/pages/channel/+page.svelte
@@ -37,41 +37,43 @@
 <main class="bg-slate-50 dark:bg-[#1d232e] min-h-screen min-w-[360px] pt-16">
   <section class="container max-w-[820px] mx-auto px-2 pt-6 pb-20 flex-col space-y-4">
     {#if isLoading}
-    <div
-      class="flex items-center justify-center w-full pt-1 pb-6 tracking-tight text-sm text-gray-500 dark:text-gray-400 font-mono"
-    >
-      loading...
-    </div>
-    {/if} {#if channel}
-    <div class="border rounded-md border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white">
       <div
-        class="flex justify-between items-center py-4 pl-5 pr-4 rounded-t border-b dark:border-gray-700"
+        class="flex items-center justify-center w-full pt-1 pb-6 tracking-tight text-sm text-gray-500 dark:text-gray-400 font-mono"
       >
-        <div class="w-1/3 overflow-hidden">
-          <h1 class="text-l font-medium text-gray-900 dark:text-white">{channel.name}</h1>
+        loading...
+      </div>
+    {/if}
+    {#if channel}
+      <div class="border rounded-md border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white">
+        <div
+          class="flex justify-between items-center py-4 pl-5 pr-4 rounded-t border-b dark:border-gray-700"
+        >
+          <div class="w-1/3 overflow-hidden">
+            <h1 class="text-l font-medium text-gray-900 dark:text-white">{channel.name}</h1>
+          </div>
+        </div>
+        <div class="overflow-y-auto overflow-x-hidden w-full p-10">
+          <HTMLPreview data={channel} />
         </div>
       </div>
-      <div class="overflow-y-scroll overflow-x-hidden w-full p-10">
-        <HTMLPreview data="{channel}" />
-      </div>
-    </div>
-    {/if} {#if streams.length}
-    <div class="border rounded-md border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white">
-      <div
-        class="flex justify-between items-center py-4 pl-5 pr-4 rounded-t border-b dark:border-gray-700"
-      >
-        <div class="w-1/3 overflow-hidden">
-          <h3 class="text-l font-medium text-gray-900 dark:text-white">Streams</h3>
+    {/if}
+    {#if streams.length}
+      <div class="border rounded-md border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white">
+        <div
+          class="flex justify-between items-center py-4 pl-5 pr-4 rounded-t border-b dark:border-gray-700"
+        >
+          <div class="w-1/3 overflow-hidden">
+            <h3 class="text-l font-medium text-gray-900 dark:text-white">Streams</h3>
+          </div>
+        </div>
+        <div class="overflow-y-auto overflow-x-hidden w-full p-6">
+          <div class="space-y-2">
+            {#each streams as stream}
+              <StreamItem {stream} />
+            {/each}
+          </div>
         </div>
       </div>
-      <div class="overflow-y-scroll overflow-x-hidden w-full p-6">
-        <div class="space-y-2">
-          {#each streams as stream}
-          <StreamItem stream="{stream}" />
-          {/each}
-        </div>
-      </div>
-    </div>
     {/if}
     <!-- {#if guides.length}
     <div class="border rounded-md border-gray-200 dark:border-gray-700 dark:bg-gray-800 bg-white">
@@ -82,7 +84,7 @@
           <h3 class="text-l font-medium text-gray-900 dark:text-white">Guides</h3>
         </div>
       </div>
-      <div class="overflow-y-scroll overflow-x-hidden w-full p-6">
+      <div class="overflow-y-auto overflow-x-hidden w-full p-6">
         <div class="space-y-2">
           {#each guides as guide}
           <GuideItem guide="{guide}" />
