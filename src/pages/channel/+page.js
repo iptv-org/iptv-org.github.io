f405657e2f00a9c95778afcfaafef3e91c29e67e
freearhey <7253922+freearhey@users.noreply.github.com>
2023-10-10 10:27:20 +0300

    Fixes prerender error

--- src/pages/channel/+page.js
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit'
-
-export function load({ url }) {
-  const id = url.searchParams.get('id')
-  const [name, country] = id.split('.')
-  const redirectUrl = `/channels/${country}/${name}`
-
-  throw redirect(302, redirectUrl)
-}
---
+++ src/pages/channel/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { redirect } from '@sveltejs/kit'
+  import { page } from '$app/stores'
+  import { onMount } from 'svelte'
+
+  onMount(() => {
+    const id = $page.url.searchParams.get('id')
+    const [name, country] = id.split('.')
+    const redirectUrl = `/channels/${country}/${name}`
+
+    window.location.replace(redirectUrl)
+  })
+</script>
+
+Redirecting...
