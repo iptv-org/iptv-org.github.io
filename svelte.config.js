e8f17e61b98c6406b3a0c1b50473ca4db5980c02
freearhey <7253922+freearhey@users.noreply.github.com>
2023-10-10 16:34:31 +0300

    Remove svelte-preprocess package

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-static'
-import sveltePreprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -16,7 +16,7 @@ const config = {
       precompress: false
     })
   },
-  preprocess: sveltePreprocess()
+  preprocess: vitePreprocess()
 }
 
 export default config
