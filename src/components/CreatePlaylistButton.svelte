3dcd5cf9c649098517c6aa14084133ee975790d9
freearhey <7253922+freearhey@users.noreply.github.com>
2023-12-06 22:43:10 +0300

    Update CreatePlaylistButton.svelte

--- src/components/CreatePlaylistButton.svelte
+++ src/components/CreatePlaylistButton.svelte
@@ -1,61 +1,34 @@
 <script>
   import { downloadMode } from '~/store'
-  import OutlineButton from '~/components/OutlineButton.svelte'
-  import ActionButton from '~/components/ActionButton.svelte'
+  import SquareButton from '~/components/SquareButton.svelte'
   import { createEventDispatcher } from 'svelte'
 
   const dispatch = createEventDispatcher()
 </script>
 
-<ActionButton
+<SquareButton
   active={$downloadMode}
   on:click={() => {
     downloadMode.set(!$downloadMode)
     dispatch('click')
   }}
-  aria-label="Create Playlist"
+  aria-label="Create playlist"
+  title="Create playlist"
 >
   <span class="inline">
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      class="w-5 h-5"
       fill="currentColor"
-      viewBox="0 0 512 512"
+      class="w-6 h-6"
+      clip-rule="evenodd"
+      fill-rule="evenodd"
+      stroke-linejoin="round"
+      stroke-miterlimit="2"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
       ><path
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="48"
-        d="M160 144h288M160 256h288M160 368h288"
-      /><circle
-        cx="80"
-        cy="144"
-        r="16"
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="32"
-      /><circle
-        cx="80"
-        cy="256"
-        r="16"
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="32"
-      /><circle
-        cx="80"
-        cy="368"
-        r="16"
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="32"
+        d="m17.5 11c2.484 0 4.5 2.016 4.5 4.5s-2.016 4.5-4.5 4.5-4.5-2.016-4.5-4.5 2.016-4.5 4.5-4.5zm.5 4v-1.5c0-.265-.235-.5-.5-.5s-.5.235-.5.5v1.5h-1.5c-.265 0-.5.235-.5.5s.235.5.5.5h1.5v1.5c0 .265.235.5.5.5s.5-.235.5-.5c0-.592 0-1.5 0-1.5h1.5c.265 0 .5-.235.5-.5s-.235-.5-.5-.5c-.592 0-1.5 0-1.5 0zm-6.479 1c.043.522.153 1.025.321 1.5h-9.092c-.414 0-.75-.336-.75-.75s.336-.75.75-.75zm1.106-4c-.328.456-.594.96-.785 1.5h-9.092c-.414 0-.75-.336-.75-.75s.336-.75.75-.75zm7.373-3.25c0-.414-.336-.75-.75-.75h-16.5c-.414 0-.75.336-.75.75s.336.75.75.75h16.5c.414 0 .75-.336.75-.75zm0-4c0-.414-.336-.75-.75-.75h-16.5c-.414 0-.75.336-.75.75s.336.75.75.75h16.5c.414 0 .75-.336.75-.75z"
+        fill-rule="nonzero"
       /></svg
     >
-  </span><span class="hidden md:inline">Create Playlist</span>
-</ActionButton>
+  </span>
+</SquareButton>
