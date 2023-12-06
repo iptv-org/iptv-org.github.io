793d0357aa5c5a6960ae9e2887e82bafd3c80891
freearhey <7253922+freearhey@users.noreply.github.com>
2023-12-06 22:43:17 +0300

    Update OutlineButton.svelte

--- src/components/OutlineButton.svelte
+++ src/components/OutlineButton.svelte
@@ -1,6 +1,6 @@
 <button
   {...$$restProps}
-  class="rounded-md bg-transparent transition-colors duration-200 border border-gray-200 hover:border-gray-300 dark:border-gray-700 dark:bg-gray-700 text-gray-700 dark:text-white dark:hover:bg-gray-600 dark:hover:border-gray-600 text-sm font-normal text-center px-4 h-10 flex items-center justify-center space-x-3"
+  class="rounded-md bg-transparent transition-colors duration-200 border border-gray-200 hover:border-gray-300 dark:border-gray-700 text-gray-700 dark:text-gray-100 dark:hover:bg-gray-700 dark:hover:border-gray-700 text-sm font-normal text-center px-4 h-10 flex items-center justify-center space-x-3"
   on:click
 >
   <slot />
