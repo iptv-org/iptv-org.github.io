faaf89aa2836b27f5c7dcba259e66b34c7a94488
freearhey <7253922+freearhey@users.noreply.github.com>
2024-11-04 05:42:10 +0300

    Fixes an error that occurred during build
    "Error: The following pages contain links to /#1", but no element with id="1"" exists on /"
    
    https://github.com/iptv-org/iptv-org.github.io/actions/runs/11647905284/job/32433702477

--- src/components/HTMLPreview.svelte
+++ src/components/HTMLPreview.svelte
@@ -80,7 +80,9 @@
   ].filter(f => (Array.isArray(f.value) ? f.value.length : f.value))
 
   function norm(value) {
-    return value.includes(' ') ? `"${value}"` : value
+    value = value.includes(' ') ? `"${value}"` : value
+
+    return encodeURIComponent(value)
   }
 </script>
 
@@ -144,7 +146,7 @@
                       stroke-linejoin="round"
                       stroke-width="2"
                       d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                    ></path>
+                    />
                   </svg>
                 </span></a
               >
