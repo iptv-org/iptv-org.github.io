5f9f599912bbca1fd29975994950500102e2fc13
freearhey <7253922+freearhey@users.noreply.github.com>
2024-12-30 18:00:00 +0300

    Create ClosedBadge.svelte

---
+++ src/components/ClosedBadge.svelte
@@ -0,0 +1,17 @@
+<script>
+  import tippy from 'sveltejs-tippy'
+
+  export let channel
+</script>
+
+<div
+  class="text-gray-500 border-[1px] border-gray-200 text-xs inline-flex items-center px-2.5 py-0.5 dark:text-gray-300 cursor-default rounded-full"
+  use:tippy={{
+    content: `closed: ${channel.closed}`,
+    allowHTML: true,
+    placement: 'right',
+    interactive: true
+  }}
+>
+  Closed
+</div>
