00ba65d198504ce5d6d1e66126bb74a13c79eacc
freearhey <7253922+freearhey@users.noreply.github.com>
2025-11-30 16:33:42 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: [vitePreprocess()],
   kit: {
-    alias: {
-      '~': 'src'
-    },
     adapter: adapter({
       pages: 'docs',
       assets: 'docs',
