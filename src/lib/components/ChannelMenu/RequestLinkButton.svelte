8c134c71a8f14236030aa27fcac81974bf028a69
freearhey <7253922+freearhey@users.noreply.github.com>
2026-04-12 02:12:51 +0300

    Create RequestLinkButton.svelte

---
+++ src/lib/components/ChannelMenu/RequestLinkButton.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { Button } from '$lib/components'
+  import type { Channel } from '$lib/models'
+  import * as Icon from '$lib/icons'
+
+  interface Props {
+    channel: Channel
+    onClick?: () => void
+  }
+
+  const { channel, onClick = () => {} }: Props = $props()
+
+  function _onClick() {
+    window.open(channel.getRequestLinkUrl(), '_blank')
+    onClick()
+  }
+</script>
+
+<Button onClick={_onClick} label="Request Stream">
+  {#snippet left()}
+    <Icon.Request class="text-gray-400" size={16} />
+  {/snippet}
+  {#snippet right()}
+    <Icon.ExternalLink class="text-gray-400 dark:text-gray-500" size={17} />
+  {/snippet}
+</Button>
