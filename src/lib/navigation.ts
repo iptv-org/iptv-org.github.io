6130b7e78a718884897c61bb579dba1f1fa027c7
freearhey <7253922+freearhey@users.noreply.github.com>
2026-04-28 13:37:45 +0300

    Update navigation.ts

--- src/lib/navigation.ts
+++ src/lib/navigation.ts
@@ -2,11 +2,19 @@ import { browser } from '$app/environment'
 import { goto } from '$app/navigation'
 
 export function setSearchParam(key?: string, value?: string) {
-  let query = key ? `?${key}=${value}` : ''
-  query = query.replace(/\+/g, '%2B')
-  const url = `${window.location.protocol}//${window.location.host}/${query}`
-  goto(url)
-  setPageTitle(value)
+  const url = new URL(window.location.href)
+
+  if (key && value) {
+    url.searchParams.set(key, value)
+  } else {
+    url.search = ''
+  }
+
+  goto(url.pathname + url.search)
+
+  if (value) {
+    setPageTitle(value)
+  }
 }
 
 export function setPageTitle(value?: string) {
